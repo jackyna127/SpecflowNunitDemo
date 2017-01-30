55d364e06599011e9953effa4229d9d04c624c61
Lina Yi <lyi@planittesting.com>
2017-01-31 10:41:10 +1300

    Command line to execute test

--- SpecFlowNUnitDemo/Reporting/Run.bat
+++ SpecFlowNUnitDemo/Reporting/Run.bat
@@ -1,2 +1,8 @@
 echo "Execute test from Nunit.Console"
 ..\..\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe --labels=On --output=TestResult.txt --result=TestResult.xml;format=nunit2 ..\bin\Debug\SpecFlowNUnitDemo.dll
+
+IF NOT EXIST TestResult.txt GOTO FAIL
+EXIT 
+:FAIL
+echo ##teamcity[buildStatus status='FAILURE']
+exit /b
